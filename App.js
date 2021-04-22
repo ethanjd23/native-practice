14bcd8dad9d81cfbcbae8c4d9a8901929b9c7290
ethanjd23 <ethanjd23@gmail.com>
2021-04-22 12:20:23 -0500

    basic done

--- App.js
+++ App.js
@@ -4,18 +4,38 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.outsideContainer}>
+      <View style={styles.middleContainer}>
+    <View style={styles.insideContainer}>
+      <Text style={styles.textStyle}>#CrushingIt</Text>
       <StatusBar style="auto" />
+    </View>
+    </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  outsideContainer: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "red",
+    padding: 20,
+  },
+  middleContainer: {
+    flex: 1,
+    backgroundColor: "black",
+    padding: 20,
+  },
+  insideContainer: {
+    flex: 1,
+    backgroundColor: 'lightgreen',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  textStyle: {
+    color: 'white',
+    fontSize: 40,
+    fontFamily: "Marker Felt",
+    backgroundColor: 'red',
   },
 });
